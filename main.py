174d2126298fd879fe0fd2530a74c25336c686ca
Mike <56518407+redmictian@users.noreply.github.com>
2021-06-12 15:37:33 +0000

    letters are better randomazed

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 from random import randrange
 letters = ['a','s','d','f','l','k','j']
 difficulty = 2
-def letterSequenceFun(quantity,letter1,letter2):
+def letterSequenceFun(quantity,letter_array):
   letterSequence = ''
   for i in range(quantity):
-      if randrange((2)) == 1:
-        letterSequence += letter1
-      else:
-        letterSequence += letter2
+    intermediate = randrange(len(letter_array))
+    letterSequence += letter_array[intermediate]
   return letterSequence
 def gameCode():
   global difficulty
@@ -22,5 +20,5 @@ def gameCode():
       print('Try again.')
 print('put your left hands pinky finger on letter A, ring finger to S, middle to D, pointing on F. Pinky finger of your right hand on ;, ring finger on L, middle one on K, pointing on J. Remember position of your fingers. Repeat after me')
 while True: 
-  rightSequence = letterSequenceFun(difficulty,letters[randrange((7))],letters[randrange((7))])
+  rightSequence = letterSequenceFun(difficulty,letters)
   gameCode()
